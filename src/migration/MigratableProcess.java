97da56559dec487be7baee701132cdc0060e93fb
Di Li <lidise7en@gmail.com>
2013-09-01 12:30:25 -0400

    Add the interface of toString()

--- src/migration/MigratableProcess.java
+++ src/migration/MigratableProcess.java
@@ -5,4 +5,6 @@ public interface MigratableProcess extends Runnable, Serializable {
 
     //functions
     void suspend(); //will be called before object is serialized
+
+    String toString(void);//get the string representation of process
 }
